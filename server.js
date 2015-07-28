e2e37f31fb94cbd087337a12efa839fb478abd4a
Vinay Gaba <vinaygaba@gmail.com>
2015-07-28 18:07:28 +0530

    Added router and requestHandler Logic

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 
 var http = require("http");
+var url = require("url");
 
-function start(){
+function start(route,handle){
 function onRequest(request,response){
-  console.log("Request Received");
+  var pathname = url.parse(request.url).pathname;
+  console.log("Request for " +pathname + " Received");
+
+  route(handle,pathname);
+
   response.writeHead(200,{"Content-Type":"text/plain"});
   response.write("Hello World");
   response.end();
